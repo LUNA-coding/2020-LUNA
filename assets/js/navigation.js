92c82d9fbb0ef30e1d34e4858e91f0c5a5acb503
suhdonghwi <hwidongsuh@gmail.com>
2020-01-16 20:06:54 +0900

    사이드 메뉴 버튼 클릭 이벤트 추가

---
+++ assets/js/navigation.js
@@ -0,0 +1,9 @@
+var menuButton = document.querySelector(".nav__side-menu-button");
+var sideMenu = document.querySelector(".nav__side-menu");
+var sideOverlay = document.querySelector(".nav__side-overlay");
+
+menuButton.addEventListener('click', function(event) {
+  sideMenu.classList.add('nav__side-menu--visible');
+  sideOverlay.classList.add('nav__side-overlay--visible');
+  console.log('wow');
+});
