671e08e340376bfc6a762fcdcf03fb74beb0b9d8
suhdonghwi <hwidongsuh@gmail.com>
2020-01-18 15:07:32 +0900

    sectionSelector 설정 변경

--- assets/js/luna.js
+++ assets/js/luna.js
@@ -1,5 +1,6 @@
-new fullpage('#fullpage', {
+new fullpage('.luna', {
   //options here
-  licenseKey: 'OPEN-SOURCE-GPL3-LICENSE',
-  fitToSection: true
+  licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+  fitToSection: true,
+  sectionSelector: '.luna__section'
 });
