17c4e653418e3b3416b5b91739861ea6fd08f0c3
suhdonghwi <hwidongsuh@gmail.com>
2020-01-18 18:37:57 +0900

    동아리원 숫자 추가

--- assets/js/luna.js
+++ assets/js/luna.js
@@ -2,4 +2,30 @@ new fullpage('.luna', {
   //options here
   licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
   sectionSelector: '.luna__section',
+  autoScrolling: true,
+  onLeave: function(origin, dest, dir) {
+    var selector = "";
+    var destNum = 0;
+    if (dest.index === 1) {
+      selector = ".second" 
+      destNum = 21;
+    }
+    else {
+      return true;
+    }
+
+    var toAnimateList = document.querySelectorAll(selector + " .to-animate");
+    console.log(toAnimateList)
+    toAnimateList.forEach(function(elem) {
+      elem.classList.add("to-animate--active")
+    });
+
+    anime({
+      targets: selector + " .number",
+      textContent: destNum,
+      easing: 'easeOutSine',
+      round: 1, 
+      duration: 3000,
+    });
+  }
 });
