763d12deece73175b2e210ff61bc09f62a6fdcef
suhdonghwi <hwidongsuh@gmail.com>
2020-01-18 16:48:32 +0900

    루나 첫 페이지 추가

--- assets/js/luna.js
+++ assets/js/luna.js
@@ -1,6 +1,5 @@
 new fullpage('.luna', {
   //options here
   licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
-  fitToSection: true,
-  sectionSelector: '.luna__section'
+  sectionSelector: '.luna__section',
 });
